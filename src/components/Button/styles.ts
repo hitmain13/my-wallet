a3a6017ef1aad8c15cf93d2d469d156d82bc8c24
Fábio Hideaki Matsumoto <hide.fabio123@gmail.com>
2022-06-12 14:59:02 -0300

    feat: adjust responsiveness

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.button`
     width: 100%;
 
-    margin: 7px 0;
+    margin: 30px 0 0 0;
     padding: 10px;
 
     border-radius: 5px;
