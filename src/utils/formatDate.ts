83f3f35405d19e0f6f052e434c334a8b36dc79fd
Fábio Hideaki Matsumoto <hide.fabio123@gmail.com>
2022-06-15 00:03:05 -0300

    adjust: format date

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -2,6 +2,7 @@ import dateFormat from 'dateformat'
 
 const formatDate = (date: string): string => {
     const pickedDate = new Date(date);
+    pickedDate.setDate(pickedDate.getDate() -1)
     return dateFormat(pickedDate, "dd/mm/yyyy");
 }
 
