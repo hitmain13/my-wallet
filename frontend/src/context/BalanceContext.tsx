b4714a0e73e815c0986ecc267d5c9352b2b058e1
Fabio Matsumoto <hide.fabio123@gmail.com>
2022-07-12 22:10:23 -0300

    feat: add gains and expenses states

--- frontend/src/context/BalanceContext.tsx
+++ frontend/src/context/BalanceContext.tsx
@@ -2,34 +2,47 @@ import React, { createContext, useState, useEffect } from "react";
 import { BalanceProps } from "../types/BalanceType";
 
 type BalanceProviderProps = {
-	// no React 18+, necessário tipagem do children através do type ou interface;
-	children?: React.ReactNode;
+  // no React 18+, necessário tipagem do children através do type ou interface;
+  children?: React.ReactNode;
 };
 
 interface IBalanceContext {
-	totalGainsAmount: BalanceProps;
-	totalExpensesAmount: BalanceProps;
+  totalGainsAmount: BalanceProps[] | [];
+  totalExpensesAmount: BalanceProps[] | [];
 }
 
-export const BalanceContext = createContext<IBalanceContext>({} as IBalanceContext);
+export const BalanceContext = createContext<IBalanceContext>(
+  {} as IBalanceContext
+);
 
-export const BalanceProvider: React.FC<BalanceProviderProps> = ({ children }) => {
-	const localGains = localStorage.getItem("@my-wallet:gains");
-	const localExpenses = localStorage.getItem("@my-wallet:expenses");
+export const BalanceProvider: React.FC<BalanceProviderProps> = ({
+  children,
+}) => {
+  const [totalGainsAmount, setTotalGainsAmount] = useState([]);
+  const [totalExpensesAmount, setTotalExpensesAmount] = useState([]);
 
-	useEffect(() => {
-		const totalGainsAmount = localGains ? localGains : {};
-		const totalExpensesAmount = localExpenses ? localExpenses : {};
-	}, [localGains, localExpenses]);
+  const localGains = localStorage.getItem("@my-wallet:gains");
+  !localGains && localStorage.setItem("@my-wallet:gains", JSON.stringify([]));
+  const gainsJSON = localGains ? JSON.parse(localGains) : [];
 
-	return (
-		<BalanceContext.Provider
-			value={{
-				totalGainsAmount,
-				totalExpensesAmount,
-			}}
-		>
-			{children}
-		</BalanceContext.Provider>
-	);
+  const localExpenses = localStorage.getItem("@my-wallet:expenses");
+  !localExpenses && localStorage.setItem("@my-wallet:expenses", JSON.stringify([]));
+  const expensesJSON = localExpenses ? JSON.parse(localExpenses) : [];
+
+  useEffect(() => {
+    setTotalGainsAmount(gainsJSON);
+    setTotalExpensesAmount(expensesJSON);
+  }, []);
+
+
+  return (
+    <BalanceContext.Provider
+      value={{
+        totalGainsAmount,
+        totalExpensesAmount,
+      }}
+    >
+      {children}
+    </BalanceContext.Provider>
+  );
 };
